da14cc699639eedbfd3e4afc26d72574e860e1e4
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-09-21 14:33:22 +0700

    hero section

---
+++ src/components/Main.js/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "../Hero/Hero";
+
+const Main = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
